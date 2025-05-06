d319986f2a328bfb9f97923e10556d59404311ad
Samuel Daligdig <daligdigsamuel812@gmail.com>
2025-05-06 11:16:02 +0800

    Refactor using class

--- script.js
+++ script.js
@@ -8,65 +8,67 @@ const closeDialog = document.querySelector('.close-dialog');
 const addBook = document.querySelector('.add-book');
 
 
-const books = [
+const myLibrary = [];
+
+const booksLibrary = [
   {
-    id: 1, 
+    id: crypto.randomUUID(), 
     title: "To Kill a Mockingbird",
     author: "Harper Lee",
     pages: 281,
     isRead: false
   },
   {
-    id: 2, 
+    id: crypto.randomUUID(), 
     title: "1984",
     author: "George Orwell",
     pages: 328,
     isRead: false
   },
   {
-    id: 3, 
+    id: crypto.randomUUID(), 
     title: "The Great Gatsby",
     author: "F. Scott Fitzgerald",
     pages: 180,
     isRead: false
   },
   {
-    id: 4, 
+    id: crypto.randomUUID(), 
     title: "Pride and Prejudice",
     author: "Jane Austen",
     pages: 279,
     isRead: false
   },
   {
-    id: 5, 
+    id: crypto.randomUUID(), 
     title: "Moby-Dick",
     author: "Herman Melville",
     pages: 635,
     isRead: false
   },
   {
-    id: 6, 
+    id: crypto.randomUUID(), 
     title: "The Catcher in the Rye",
     author: "J.D. Salinger",
     pages: 214,
     isRead: false
   },
   {
-    id: 7, 
+    id: crypto.randomUUID(), 
     title: "Brave New World",
     author: "Aldous Huxley",
     pages: 268,
     isRead: false
   },
   {
-    id: 8, 
+    id: crypto.randomUUID(), 
     title: "The Hobbit",
     author: "J.R.R. Tolkien",
     pages: 310,
     isRead: false
   },
   {
-    id: 9, 
+    id: crypto.randomUUID(), 
     title: "Fahrenheit 451",
     author: "Ray Bradbury",
     pages: 194,
@@ -74,56 +76,110 @@ const books = [
   }
 ];
 
-let uniqueBookId = books[books.length - 1].id;
+let latestVal = 0;
 
-const resetBook = () => {
-  bottomMain.innerHTML = '';
+class Book {
+   constructor(id, title, author, pages, isRead) {
+      this.id = id;
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.isRead = isRead;
+   }
+
+   addOneBook(library) {
+    library.push({
+      id: this.id, 
+      title: this.title,
+      author: this.author,
+      pages: this.pages,
+      isRead: this.isRead
+    });
+    //
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book';
+    bookDiv.id = this.id;
+    bookDiv.setAttribute('data-index-value', latestVal);
+
+    const titleDiv = document.createElement('div');
+    titleDiv.className = 'title';
+    titleDiv.textContent = this.title;
+
+    const authorDiv = document.createElement('div');
+    authorDiv.className = 'author';
+    authorDiv.textContent = this.author;
+
+    const pageDiv = document.createElement('div');
+    pageDiv.className = 'page';
+    pageDiv.textContent = this.pages;
+
+    const svgDiv = document.createElement('div');
+    svgDiv.className = 'action-btn';
+
+    const readBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+    readBtn.textContent = 'Not Read';
+    readBtn.value = 'Read';
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.value = 'Delete';
+    deleteBtn.setAttribute('data-index-value', latestVal);
+
+    svgDiv.appendChild(readBtn);
+    svgDiv.appendChild(deleteBtn);
+
+    bookDiv.appendChild(titleDiv);
+    bookDiv.appendChild(authorDiv);
+    bookDiv.appendChild(pageDiv);
+    bookDiv.appendChild(svgDiv);
+    bottomMain.appendChild(bookDiv);
+    //
+   latestVal++;
+   }
 }
 
-const createBook = (bookId, bookTitle, bookAuthor, bookPages, isRead) => {
-  let saveSvg = isRead ? `<svg onclick='toReadBook(${bookId})' class='read' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Done read!</title><path onclick='toReadBook(${bookId})' d="M16.75 22.16L14 19.16L15.16 18L16.75 19.59L20.34 16L21.5 17.41L16.75 22.16M18 2C19.1 2 20 2.9 20 4V13.34C19.37 13.12 18.7 13 18 13V4H13V12L10.5 9.75L8 12V4H6V20H12.08C12.2 20.72 12.45 21.39 12.8 22H6C4.9 22 4 21.1 4 20V4C4 2.9 4.9 2 6 2H18Z" /></svg>` : `<svg onclick='toReadBook(${bookId})' class='not-read'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="is-read"><title>Not read yet</title><path onclick='toReadBook(${bookId})' d="M18,2A2,2 0 0,1 20,4V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V4A2,2 0 0,1 6,2H18M18,4H13V12L10.5,9.75L8,12V4H6V20H18V4Z" /></svg>`;
-  return `
-      <div class="book" id="${bookId}">
-        <div class="title">${bookTitle}</div>
-        <div class="author">- ${bookAuthor}</div>
-        <div class="page">${bookPages} pages</div>
-        <div class="action-btn">
-          ${saveSvg}
-          <svg onclick='toDeleteBook(${bookId})' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="delete ${bookId}"><title>Delete ${bookTitle}?</title><path onclick='toDeleteBook(${bookId})' d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" class="delete"/></svg>
-        </div>
-      </div>
-    `;
+let book = new Book();
+
+const generateBook = (library) => {
+  library.forEach((bookItem, bookIndex) => {
+    let addBooks = new Book(crypto.randomUUID(), bookItem.title, bookItem.author, bookItem.pages, bookItem.isRead);
+    addBooks.addOneBook(myLibrary);
+  });
 }
 
-const toDeleteBook = (bookId) => {
-  books.filter((item, index) => {
-    if (item.id === bookId) {
-      books.splice(index, 1);
-      updateBook();
-      return;
+generateBook(booksLibrary);
+
+const toRead = (target, color, text) => {
+  target.style.backgroundColor = color;//'var(--bg-color)'
+  target.textContent = text;//'Not Read'
+}
+
+const toggleRead = (target) => {
+  if (target.textContent === 'Done Read') {
+    toRead(target, 'var(--bg-color)', 'Not Read');
+    return;
+  }
+
+  if (target.textContent === 'Not Read') {
+    toRead(target, 'var(--read-btn)', 'Done Read');
+    return;
+  }
+}
+
+const toggleDialog = () => {
+  dialog.open ? root.style.setProperty('--display-form', 'none') : root.style.setProperty('--display-form', 'flex');
+  dialog.open = !dialog.open;
+}
+
+const removeEl = (target) => {
+  const bookList = document.querySelectorAll('.book');
+
+  bookList.forEach((listItem, listIndex) => {
+    if (listItem.dataset.indexValue === target.dataset.indexValue) {
+      listItem.remove();
+      myLibrary.splice(listIndex, 1);
     }
   });
 }
-
-const toReadBook = (bookId) => {
-  books.filter((item, index) => {
-    if (item.id === bookId) {
-      item.isRead = !item.isRead;
-      updateBook();
-      return;
-    }
-  });
-}
-
-
-const updateBook = () => {
-  resetBook();
-  books.forEach(bookItem => {
-    bottomMain.innerHTML += createBook(bookItem.id, bookItem.title, bookItem.author, bookItem.pages, bookItem.isRead);
-  });
-}
-
-updateBook();
 
 const toAddBook = () => {
   let title = getInput[0].value;
@@ -134,27 +190,56 @@ const toAddBook = () => {
     return;
   }
 
-  uniqueBookId++;
+  const newBook = new Book(crypto.randomUUID(), title, author, pages, false);
+  newBook.addOneBook(myLibrary);
+  toggleDialog();
+  getInput[0].value = '';
+  getInput[1].value = '';
+  getInput[2].value = '';
+}
 
-  let newBook = {
-    id: uniqueBookId, 
-    title: title,
-    author: author,
-    pages: pages,
-    isRead: false
+let bookSize = 0;//px
+
+const bookWidth = (width) => {
+  root.style.setProperty('--book-grid-template', `repeat(auto-fit, minmax(14rem, ${width}))`);
+}
+
+const bookSizes = () => {
+  const bookList = document.querySelectorAll('.book');
+  bookSize = 0;
+  bookSize = Math.floor(bookList[0].offsetWidth) - 0.5;
+  console.log(bookSize);
+
+  if (myLibrary.length <= 4) {
+    bookWidth(`${bookSize}px`);
+  } else {
+    bookWidth(`1fr`);
+  }
+}
+
+window.addEventListener('resize', (e) => {
+  bookSizes();
+});
+
+document.addEventListener('click', event => {
+  const eTarget = event.target;
+
+  if (eTarget.value === 'Delete') {
+    removeEl(eTarget);
   }
 
-  books.push(newBook);
-  updateBook();
-  getInput.forEach(inputItem => inputItem.value = '');
-}
+  if (eTarget.value === 'Read') {
+    toggleRead(eTarget);
+  }
 
-toAddBook();
+  if (eTarget.className === 'add-book') {
+    toAddBook();
+  }
 
-const toggleDialog = () => {
-  dialog.open ? root.style.setProperty('--display-form', 'none') : root.style.setProperty('--display-form', 'flex');
-  dialog.open = !dialog.open;
-}
+  bookSizes();
+});
+
+
 
 addNewBook.addEventListener('click', (e) => {
   toggleDialog();
@@ -163,30 +248,3 @@ addNewBook.addEventListener('click', (e) => {
 closeDialog.addEventListener('click', (e) => {
   toggleDialog();
 });
-
-addBook.addEventListener('click', (e) => {
-  getInput.forEach(inputItem => {
-    if (inputItem.value === '') {
-      return;
-    }
-  });
-  toAddBook();
-  toggleDialog();
-})
-
-
-document.addEventListener('click', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-
-  if (books.length < 5) {
-    root.style.setProperty('--book-grid-template', 'repeat(auto-fit, 23.7%');
-    return;
-  } else {
-    root.style.setProperty('--book-grid-template', 'repeat(auto-fit, minmax(23.7%, 1fr))');
-    return;
-  }
-})
-
-
-
